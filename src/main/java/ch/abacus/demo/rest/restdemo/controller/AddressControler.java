6485ed30afc68d2be4b86ebade4bca3d9bd5fb72
Dani <daniel.schneider@abacus.ch>
2019-06-06 10:18:28 +0200

    Erweiterung und Port=8088

--- src/main/java/ch/abacus/demo/rest/restdemo/controller/AddressControler.java
+++ src/main/java/ch/abacus/demo/rest/restdemo/controller/AddressControler.java
@@ -4,17 +4,22 @@ import ch.abacus.demo.rest.restdemo.model.Address;
 import ch.abacus.demo.rest.restdemo.repository.AddressRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Collection;
+import java.util.Collections;
 
 @Component
 @Path("/addresses")
@@ -27,45 +32,144 @@ public class AddressControler {
         this.addressRepository = addressRepository;
     }
 
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/test")
     public String sayHello() {
-
+        // HTTP-Statuscode 200 OK
         return "Hello du";
     }
 
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public Collection<Address> getAddresses(@QueryParam("name") String name,
+                                            @QueryParam("surname") String surname,
+                                            @QueryParam("zip") String zip,
+                                            @QueryParam("city") String city) {
+        // HTTP-Statuscode 200 OK
+        return addressRepository.getAllAddresses(name, surname, zip, city);
+    }
+
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Collection<Address> getAddresses() {
+    @Path("/{id}")
+    public Response getAddress(@PathParam("id") Long id) {
+        Address address = addressRepository.findById(id);
+        if(address!=null){
+            // HTTP-Statuscode 200 OK
+            return Response.status(Response.Status.OK).entity(address).build();
+        }
+        else {
+            // HTTP-Statuscode 404 NOT_FOUND
+            // Die angeforderte Ressource wurde nicht gefunden.
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
 
-        return addressRepository.getAllAddresses();
+//    // einfache implementierung
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    @Path("/{id}")
+//    public Address getAddress(@PathParam("id") Long id) {
+//        // HTTP-Statuscode 200 OK  oder
+//        // HTTP-Statuscode 204 NO_CONTENT
+//        return addressRepository.findById(id);
+//    }
+
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/{id}/name")
+    public String getName(@PathParam("id") Long id) {
+        // HTTP-Statuscode 200 OK  oder
+        // HTTP-Statuscode 500 INTERNAL_SERVER_ERROR
+        return addressRepository.findById(id).getName();
     }
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("/{oid}")
-    public Address getAddress(@PathParam("oid") Long id) {
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/{id}/surname")
+    public Response getSurname(@PathParam("id") Long id) {
+        if(addressRepository.exist(id)) {
+            // HTTP-Statuscode 200 OK
+            String surName = addressRepository.findById(id).getSurname();
+            return Response.status(Response.Status.OK).entity(surName).build();
+        }
+        else {
+            // HTTP-Statuscode 404 NOT_FOUND
+            // Die angeforderte Ressource wurde nicht gefunden.
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
 
-        return addressRepository.findById(id);
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/{id}/zip")
+    public String getZip(@PathParam("id") Long id) {
+        return addressRepository.findById(id).getZip();
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/{id}/city")
+    public String getCity(@PathParam("id") Long id) {
+        return addressRepository.findById(id).getCity();
     }
 
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response saveAddress(Address address) {
-        final Address save = addressRepository.save(address);
-        return Response.status(Response.Status.CREATED)
-                .entity(save)
-                .build();
-
+        if(addressRepository.exist(address.getId())){
+            // HTTP-Statuscode 405 METHOD_NOT_ALLOWED
+            // Die Anfrage darf nur mit anderen HTTP-Methoden (zum Beispiel GET statt POST) gestellt werden.
+            // Gültige Methoden für die betreffende Ressource werden im „Allow“-Header-Feld der Antwort übermittelt.
+            return Response.status(Response.Status.METHOD_NOT_ALLOWED).build();
+        }
+        else {
+            // HTTP-Statuscode 201 CREATED
+            // Die Anfrage wurde erfolgreich bearbeitet.
+            // Die angeforderte Ressource wurde vor dem Senden der Antwort erstellt.
+            final Address save = addressRepository.save(address);
+            return Response.status(Response.Status.CREATED)
+                    .entity(save)
+                    .build();
+        }
     }
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    @Path("/{oid}/name")
-    public String getName(@PathParam("oid") Long id) {
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{id}")
+    public Response updateAddress(@PathParam("id") Long id, Address address) {
+        if ( !addressRepository.exist(id)) {
+            // HTTP-Statuscode 404 NOT_FOUND
+            // Die angeforderte Ressource wurde nicht gefunden.
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        else if( !id.equals(address.getId()) ){
+            // HTTP-Statuscode 409 CONFLICT
+            // Die Anfrage wurde unter falschen Annahmen gestellt.
+            return Response.status(Response.Status.CONFLICT).build();
+        }
+        else {
+            final Address save = addressRepository.save(address);
+            // HTTP-Statuscode 200 OK
+            return Response.status(Response.Status.OK)
+                    .entity(save)
+                    .build();
+        }
+    }
 
-        return addressRepository.findById(id).getName();
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{id}")
+    public Address deleteAddress(@PathParam("id") Long id){
+        // HTTP-Statuscode 200 OK  oder
+        // HTTP-Statuscode 204 NO_CONTENT
+        return addressRepository.delete(id);
     }
 }
--- src/main/java/ch/abacus/demo/rest/restdemo/repository/AddressRepository.java
+++ src/main/java/ch/abacus/demo/rest/restdemo/repository/AddressRepository.java
@@ -3,7 +3,11 @@ package ch.abacus.demo.rest.restdemo.repository;
 import ch.abacus.demo.rest.restdemo.model.Address;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Component
 public class AddressRepository {
@@ -15,37 +19,41 @@ public class AddressRepository {
         fillWithTestData();
     }
 
-    public Collection<Address> getAllAddresses() {
+    public boolean exist(Long id){
+        return addresses.containsKey(id);
+    }
 
-        return addresses.values();
-
+    public Collection<Address> getAllAddresses(String filterName,
+                                               String filterSurname,
+                                               String filterZip,
+                                               String filterCity) {
+        List<Address> addresses = new ArrayList<>();
+        addresses.addAll(this.addresses.values());
+        if(filterName!=null){
+            addresses.removeIf(address -> !address.getName().equals(filterName));
+        }
+        if(filterSurname!=null){
+            addresses.removeIf(address -> !address.getSurname().equals(filterSurname));
+        }
+        if(filterZip!=null){
+            addresses.removeIf(address -> !address.getZip().equals(filterZip));
+        }
+        if(filterCity!=null){
+            addresses.removeIf(address -> !address.getZip().equals(filterCity));
+        }
+        return new ArrayList<>(addresses);
     }
 
     public Address findById(Long id) {
         return addresses.get(id);
     }
 
-    public void delete(Long id) {
-        addresses.remove(id);
-    }
-
-    private void fillWithTestData() {
-
-        Address address = new Address();
-        address.setSurname("Hans");
-        address.setName("Muster");
-        address.setCity("Wittenbach");
-        address.setZip("9300");
-
-        save(address);
-
+    public Address delete(Long id) {
+        return addresses.remove(id);
     }
 
     public Address save(Address address) {
-
-        boolean exits = addresses.containsKey(address.getId());
-
-        if (exits) {
+        if (exist(address.getId())) {
             addresses.put(address.getId(), address);
 
         } else {
@@ -54,5 +62,23 @@ public class AddressRepository {
             addresses.put(newId, address);
         }
         return address;
+    }
+
+    private void fillWithTestData() {
+        addAddress("Hans","Mueller",9300,"Wittenbach");
+        addAddress("Haidi","Mueller",9300,"Wittenbach");
+        addAddress("Fritz","Bauer",9300,"Wittenbach");
+        addAddress("Max","Meier",9000,"St. Gallen");
+
+    }
+
+    private void addAddress(String surname, String name, int zip, String city){
+        Address address = new Address();
+        address.setSurname(surname);
+        address.setName(name);
+        address.setZip(String.valueOf(zip));
+        address.setCity(city);
+
+        save(address);
     }
 }
