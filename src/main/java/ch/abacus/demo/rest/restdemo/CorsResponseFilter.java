b460bb62e7b7ed6cb58cdd06528f664b6927acef
c.schwyter@gmail.com <3pw-RFd-BoF-Rov>
2019-06-07 09:12:13 +0200

    add delete method

--- src/main/java/ch/abacus/demo/rest/restdemo/CorsResponseFilter.java
+++ src/main/java/ch/abacus/demo/rest/restdemo/CorsResponseFilter.java
@@ -27,7 +27,6 @@ public class CorsResponseFilter implements ContainerResponseFilter {
         MultivaluedMap<String, Object> headers = responseContext.getHeaders();
 
         headers.add("Access-Control-Allow-Origin", "*");
-        //headers.add("Access-Control-Allow-Origin", "http://abcd.org"); //allows CORS requests only coming from abcd.org
         headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
         headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Codingpedia");
 
